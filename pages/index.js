61ab8a9b941b65c8d0d54493e8d851e22ea0fe9c
Alan Peter <alanpalamattam2@gmail.com>
2025-11-03 16:58:59 +0530

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import Blogsection from "@/components/Blogsection";
 import Header from "@/components/Header";
 import { useState } from "react";
+import Head from "next/head";
+
 
 export default function Home({ blogs }) {
    const [searchQuery, setSearchQuery] = useState("");
   return ( 
+    <>
+    <Head>
+        <title>Horizon Blog | Travel, Culinary & Lifestyle Stories</title>
+        <meta
+          name="description"
+          content="Explore  blog for travel tips,destinations, culinary guides, lifestyle inspiration, and creative insights that fuel your adventures."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     <div className="p-2 flex flex-col items-center justify-center bg-gray-100 min-h-screen">
       <div className="w-full max-w-[1100px] mx-auto bg-white p-3 rounded-xl shadow-md">
          <Header onSearch={setSearchQuery} />
         <Blogsection blogs={blogs} searchQuery={searchQuery}/>
       </div>
     </div>
+    </>
   );
 }
 
